59d9c9b6748ce36fdc726388d8a3484edecddec8
rayyjeb <rajabshoukathis@gmail.com>
2024-03-18 16:29:50 +0530

    notification

--- GYMSYNC/src/App.jsx
+++ GYMSYNC/src/App.jsx
@@ -4,16 +4,19 @@ import Sidebar from "./components/Sidebar";
 import History from "./pages/History/History";
 import HomePage from "./pages/HomePage/HomePage";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import Notification from "./pages/Notification/Notification";
 function App() {
   return (
     <>
       <Navbar />
       <Sidebar />
-      {/* <HomePage /> */}
-      <ChakraProvider>
+      <HomePage />
+      {/* <ChakraProvider>
         <History />
-      </ChakraProvider>
+      </ChakraProvider> */}
+      {/* <ChakraProvider>
+        <Notification />
+      </ChakraProvider> */}
     </>
   );
 }
