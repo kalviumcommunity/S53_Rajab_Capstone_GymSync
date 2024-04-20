75ff48c646f7a1f7cb2825d82624283747f88abf
rayyjeb <rajabshoukathis@gmail.com>
2024-04-20 11:48:52 +0530

    front-end done

---
+++ Server/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./db/connectDb.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+dotenv.config();
+connectDb();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json()); //to parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data in the req.body
+app.use(cookieParser());
+
+//Routes
+app.use("/api/users", userRoutes);
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT} `);
+});
