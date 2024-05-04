fefca39331423678ea401cb657f0272dca35d9d9
rayyjeb <rajabshoukathis@gmail.com>
2024-05-04 16:42:20 +0530

    Backend deploy

--- Server/src/routes/postRoutes.js
+++ Server/src/routes/postRoutes.js
@@ -2,6 +2,6 @@ import express from "express";
 import { createPost, getFeedPosts } from "../controllers/postController.js";
 import protectRoute from "../middleware/protectRoute.js";
 const postRoutes = express.Router();
-postRoutes.get("/feed",protectRoute, getFeedPosts);
-postRoutes.post("/create", protectRoute, createPost);
+postRoutes.get("/feed", getFeedPosts);
+postRoutes.post("/create", createPost);
 export default postRoutes;
