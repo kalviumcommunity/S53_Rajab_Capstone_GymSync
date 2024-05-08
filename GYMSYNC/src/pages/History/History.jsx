6ded00871d442eae2ad342236d8080328691fef6
rayyjeb <rajabshoukathis@gmail.com>
2024-05-08 15:39:30 +0530

    Bug Fix 2 + Feature add

--- GYMSYNC/src/pages/History/History.jsx
+++ GYMSYNC/src/pages/History/History.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchNormal1 } from "iconsax-react";
 import {
   Accordion,
@@ -13,7 +13,30 @@ import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
 import MobileNavbar from "../../components/MobileNavbar";
 import { useEffect } from "react";
+
 const History = () => {
+  const userDataString = localStorage.getItem("user-gymSync");
+  const userData = JSON.parse(userDataString);
+  const userId = userData._id;
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    const getHistory = async () => {
+      try {
+        const res = await fetch(`/api/history/historydata/${userId}`);
+        const data = await res.json();
+        if (data.error) {
+          alert(data.error);
+          return;
+        }
+        setHistory(data);
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+    getHistory();
+  }, [userId]);
+
   return (
     <>
       <Navbar />
@@ -28,7 +51,6 @@ const History = () => {
                 <div className="childTopContent">
                   <div className="topContentTexts historyTopTexts">
                     <h1>HISTORY LOGS</h1>
-                    {/* <p>Let's Crush Today's Workout!</p> */}
                   </div>
                   <div className="searchbarDiv">
                     <input
@@ -59,7 +81,7 @@ const History = () => {
                           textAlign="left"
                           className="BoxAccordion"
                         >
-                          07 MARCH 2024
+                          WORKOUTS
                         </Box>
                         <AccordionIcon />
                       </AccordionButton>
@@ -69,57 +91,49 @@ const History = () => {
                         <hr className="accordionHr"></hr>
                       </div>
                       <div className="accordionTextParent">
-                        <div className="accordionTextChild">
-                          <div className="leftTexts">
-                            <div className="serialNo">
-                              <h1>S.No</h1>
-                              <br></br>
-                              <p>1</p>
-                              <p>2</p>
-                              <p>3</p>
-                              <p>4</p>
-                              <p>5</p>
-                            </div>
-                            <div className="workoutName">
-                              <h1>WORKOUT </h1>
-                              <br></br>
-                              <p>PUSH UP</p>
-                              <p>PUSH UP</p>
-                              <p>PUSH UP</p>
-                              <p>PUSH UP</p>
-                              <p>PUSH UP</p>
-                            </div>
+                        <div className="topDivHist">
+                          <div className="leftHead">
+                            <h1>S.no</h1>
+                            <h1>Workout Name</h1>
                           </div>
-                          <div className="rightTexts">
-                            <div className="serialNo">
-                              <h1>REPS</h1>
-                              <br></br>
-                              <p>15</p>
-                              <p>15</p>
-                              <p>15</p>
-                              <p>15</p>
-                              <p>15</p>
-                            </div>
-                            <div className="workoutName">
-                              <h1>SETS </h1>
-                              <br></br>
-                              <p>4</p>
-                              <p>4</p>
-                              <p>4</p>
-                              <p>4</p>
-                              <p>4</p>
-                            </div>
-                            <div className="workoutName">
-                              <h1>WEIGHT </h1>
-                              <br></br>
-                              <p>4</p>
-                              <p>4</p>
-                              <p>4</p>
-                              <p>4</p>
-                              <p>4</p>
-                            </div>
+                          <div className="rightHead">
+                            <h1>Reps</h1>
+                            <h1>Sets</h1>
+                            <h1>Weight</h1>
                           </div>
                         </div>
+                        {history && history.length > 0 ? (
+                          history.map((data, i) => {
+                            const serialNo = i + 1;
+                            return (
+                              <div key={i} className="accordionTextChild">
+                                <div className="leftTexts">
+                                  <div className="serialNo">
+                                    <p>{serialNo}</p>
+                                  </div>
+                                  <div className="workoutName">
+                                    <p>{data.workoutName}</p>
+                                  </div>
+                                </div>
+                                <div className="rightTexts">
+                                  <div className="serialNo">
+                                    <p>{data.reps}</p>
+                                  </div>
+                                  <div className="workoutName">
+                                    <p>{data.sets}</p>
+                                  </div>
+                                  <div className="workoutName">
+                                    <p>{data.weight}</p>
+                                  </div>
+                                </div>
+                              </div>
+                            );
+                          })
+                        ) : (
+                          <div className="noDataMessage">
+                            No history data available
+                          </div>
+                        )}
                       </div>
                     </AccordionPanel>
                   </AccordionItem>
