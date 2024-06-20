3e5be91645737ba0e77e8112df2becb979ae3cfe
rayyjeb <rajabshoukathis@gmail.com>
2024-06-20 16:50:59 +0530

    Backend Deployment

--- GYMSYNC/vite.config.js
+++ GYMSYNC/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],  server: {
+  plugins: [react()],
+  server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://gymsync-server.vercel.app",
+        target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
-
--- Server/utils/generateTokenandSetCookie.js
+++ Server/utils/generateTokenandSetCookie.js
@@ -7,6 +7,8 @@ const generateTokenandSetCookie = (userId, res)=>{
         httpOnly: true,
         maxAge: 15*24*60*60*1000,
         sameSite:"strict",
+        secure: process.env.NODE_ENV !== "development"
+
     })
     return token
 }
