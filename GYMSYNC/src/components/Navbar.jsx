713e10ba13d1c3cf9d0a2771ee446e300306c338
rayyjeb <rajabshoukathis@gmail.com>
2024-06-21 18:06:44 +0530

    logout added

--- GYMSYNC/src/components/Navbar.jsx
+++ GYMSYNC/src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Components.css";
 import { Link } from "react-router-dom";
+import Cookies from "js-cookie"; 
 
 const Navbar = () => {
+  const handleLogout = () => {
+    Cookies.remove("jwt"); 
+    localStorage.removeItem("user-gymSync");
+
+    console.log("After removal:", Cookies.get("jwt"));
+    window.location.href = "/auth";
+  };
+
   return (
     <div className="topBarParent">
       <div className="topBar">
@@ -10,16 +19,19 @@ const Navbar = () => {
           <img
             className="logo"
             src="https://i.imgur.com/z6spKK9_d.webp?maxwidth=760&fidelity=grand"
-          ></img>
+            alt="Logo"
+          />
         </div>
-        <div>
-          <Link to="/signup" style={{ textDecoration: "none" }}>
-            <img
-              className="userProfile"
-              src="https://cdn-icons-png.freepik.com/512/219/219969.png"
-              style={{ width: "40px" }}
-            ></img>
-          </Link>
+        <div
+          onClick={handleLogout}
+          style={{ cursor: "pointer", textDecoration: "none" }}
+        >
+          <img
+            className="userProfile"
+            src="https://icons.iconarchive.com/icons/graphicloads/100-flat-2/256/inside-logout-icon.png"
+            style={{ width: "40px" }}
+            alt="Logout"
+          />
         </div>
       </div>
     </div>
